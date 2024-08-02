f2c5f13b67c79ce64bbe4343682223b65d7ee5df
Soltyk Maksym <maxwell88soltyk@gmail.com>
2024-08-02 13:48:08 +0300

    save

---
+++ src/lib/ui/OutlinedButton/OutlinedButton.tsx
@@ -0,0 +1,24 @@
+import clsx from "clsx";
+import { OutlinedButtonProps } from "./OutlinedButtonTypes";
+
+// Better to use Button.variant === "outlined", but in this case good enough
+export function OutlinedButton({
+  children,
+  className,
+  ...props
+}: OutlinedButtonProps) {
+  return (
+    <button
+      type="button"
+      className={clsx(
+        "border border-solid rounded-lg py-[3px] px-2 text-xs/5 box-border",
+        "border-black/20 text-light-label-light-secondary-text",
+        "dark:border-[#D1D1D11F] dark:text-light-label-dark-main-text", // TODO: No color variable in design
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
---
+++ src/lib/ui/OutlinedButton/OutlinedButtonTypes.ts
@@ -0,0 +1,4 @@
+import React from "react";
+
+export interface OutlinedButtonProps
+  extends React.HTMLAttributes<HTMLButtonElement> {}
